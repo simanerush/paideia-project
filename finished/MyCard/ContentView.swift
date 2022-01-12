0bd30b2d4b6a6d0f084f2b0e59b32161b6f225c8
Sima Nerush <2002ssn@gmail.com>
2022-01-11 18:21:16 -0800

    Add finished project

--- finished/MyCard/ContentView.swift
+++ finished/MyCard/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.90, green: 0.49, blue: 0.13, opacity: 0.8)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("MyCard")
+                    .foregroundColor(.white)
+                    .font(Font.custom("OpenSansCondensed-Light", size: 20))
+                Spacer()
+                Image("sima")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 190, height: 190)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Sima Nerush")
+                    .foregroundColor(.white)
+                    .font(Font.custom("DancingScript-Bold", size: 50))
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.title3)
+                Divider()
+                ContactView(text: "123-456-78-90", image: "phone")
+                ContactView(text: "serafiman@reed.edu", image: "envelope")
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct ContactView: View {
+    var text: String
+    var image: String
+
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(.white)
+            .frame(height: 50)
+            .overlay(HStack {
+            Image(systemName: image)
+                .foregroundColor(.orange)
+            Text(text)
+                .foregroundColor(.black)
+
+        })
+        .padding(.all)
+
     }
 }
 
