af81858bf5ab671f494b72622c9197eef47e6971
Sima Nerush <2002ssn@gmail.com>
2022-01-11 22:12:03 -0800

    Add starter project

---
+++ starter/MyCard-Demo/MyCard-Demo/MyCard_DemoApp.swift
@@ -0,0 +1,22 @@
+//
+//  MyCard_DemoApp.swift
+//  MyCard-Demo
+//
+//  Created by Serafima Nerush on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCard_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// MARK: - Notes for class
+// "OpenSansCondensed-Light"
+// "DancingScript-Bold"
+// Color(red: 0.90, green: 0.49, blue: 0.13, opacity: 0.8)
